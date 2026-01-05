f4d01eade580e13da79841fd266e68177ee0b81e
anoopreddy-padi <anoopreddy.padi@gmail.com>
2026-01-05 12:17:21 +0530

    adding docker compose

--- src/main/java/com/example/patient/PatientController.java
+++ src/main/java/com/example/patient/PatientController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,9 @@ public class PatientController {
     @GetMapping("/getAllPatients")
     public String getAllPatients(){
         return "All Patients";
+    }
+    @GetMapping("/getPatient/{id}")
+    public String getAllPatients(@PathVariable int id){
+        return "Patient-"+id;
     }
 }
